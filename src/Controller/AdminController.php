45de06885136e0b1bc6db75e57563686cf3a82a9
Asraou Ismail <asraou.ismail@gmail.com>
2022-03-09 16:03:43 +0100

    feature: admin panel added successfully

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Property;
+use App\Form\PropertyType;
+use App\Repository\PropertyRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+
+     public function __construct(PropertyRepository $repository, ManagerRegistry $em) {
+        $this->em = $em;
+         $this->repository = $repository;
+        
+     }
+
+
+    /**
+     * @Route("/admin", name="admin.property.index")
+     */
+    public function index(): Response
+    {
+        $properties = $this->repository->findAll();
+
+        dump($properties);
+        return $this->render('admin/index.html.twig', [
+            'properties' => $properties,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/property/new", name="admin.property.create")
+     */
+    public function newProperty(Request $request, ManagerRegistry $em): Response
+    {
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+
+       if ($form->isSubmitted() && $form->isValid()) {
+
+           $em = $em->getManager();
+
+           $em->persist($property);
+           $em->flush();
+
+           return $this->redirectToRoute('admin.property.index');
+       }
+
+        return $this->render('admin/edit/new.html.twig', [
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+
+
+    /**
+     * @Route("/admin/edit/{id}", name="admin.property.edit", methods="GET|POST")
+     * @param Property $property
+     * @param Request $request
+     */
+    public function edit(Property $property, Request $request, ManagerRegistry $em): Response
+    {
+        $form = $this->createForm(PropertyType::class, $property);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
+            $em = $em->getManager();
+            $em->flush();
+            $this->addFlash('success', 'le Bien a etait modifié avec succès');
+
+            return $this->redirectToRoute('admin.property.index');
+
+            
+        }
+
+        return $this->render('admin/edit/_edit_form.hmtl.twig', [
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     *
+     * @Route("/admin/property/{id}", name="admin.property.delete")
+     */
+    public function delete(Property $property, Request $request, ManagerRegistry $em): Response
+    {
+
+        if($this->isCsrfTokenValid('delete' . $property->getId(), $request->get('_token'))) {
+
+            $em = $em->getManager();
+            $em->remove($property);
+            $em->flush();
+            $this->addFlash('success', 'Bien supprimé avec succès');
+            
+        }
+
+        return $this->redirectToRoute('admin.property.index');
+    }
+
+}
---
+++ src/Form/PropertyType.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Property;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PropertyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('surface')
+            ->add('rooms')
+            ->add('bedrooms')
+            ->add('floor')
+            ->add('heat', ChoiceType::class, [
+                'choices' => $this->getChoices()
+            ])
+            ->add('address')
+            ->add('postalCode')
+            ->add('city')
+            ->add('sold')
+            ->add('parking', ChoiceType::class,[
+                'choices' => $this->getParking()
+            ] )
+            ->add('status', ChoiceType::class, [
+                'choices' => $this->getStatus()
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => $this->getType()
+            ])
+            ->add('price')
+            ->add('country')
+            // ->add('createdAt')
+            // ->add('updatedAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+        ]);
+    }
+
+     public function getChoices()
+    {
+        $choices = Property::HEAT;
+        $output = [];
+        foreach($choices as $k => $v) {
+            $output[$v] = $k;
+        }
+        return $output;
+    }
+    
+      public function getStatus()
+    {
+        $choices = Property::STATUS;
+        $output = [];
+        foreach($choices as $k => $v) {
+            $output[$v] = $k;
+        }
+        return $output;
+    }
+
+     public function getType()
+    {
+        $choices = Property::TYPE;
+        $output = [];
+        foreach($choices as $k => $v) {
+            $output[$v] = $k;
+        }
+        return $output;
+    }
+
+     public function getParking()
+    {
+        $choices = Property::PARKING;
+        $output = [];
+        foreach($choices as $k => $v) {
+            $output[$v] = $k;
+        }
+        return $output;
+    }
+
+}
